10df18474bd4b05b078b11307af9d5b72b3bade6
Vladimir Kosinov <kosinovladimir@mail.ru>
2022-11-11 17:45:31 +0300

    lab_2

---
+++ lab_3.py
@@ -0,0 +1,14 @@
+a = int(input('Vvedite celoe chislo \n'))
+b = int(input('Vvedite celoe chislo \n'))
+c = int(input('Vvedite celoe chislo \n'))
+
+left = 1
+right = 3
+
+if left <= a & a >= right:
+    print(a)
+if left <= b & b >= right:
+    print(b)
+if left <= c & c >= right:
+    print(c)
+
